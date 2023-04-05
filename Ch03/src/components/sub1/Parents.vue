fa25954919f6af7c72a326a87d4af875328b8713
a01074882716 <a01074882716@gmail.com>
2023-04-05 11:48:07 +0900

    2023-04-05

---
+++ Ch03/src/components/sub1/Parents.vue
@@ -0,0 +1,15 @@
+<template>
+    <h4>Parents</h4>
+    <p>부모 컴포넌트</p>
+    <Child></Child>
+</template>
+<script>
+import Child from './Child.vue';
+
+export default {
+    name: "Parent",
+    components: { Child }
+}
+</script>
+<style scoped>
+</style>
