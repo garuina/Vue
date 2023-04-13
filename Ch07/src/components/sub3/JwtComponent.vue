65dce9932e3676c8c5bcac1c052e33c9d1026be7
a01074882716 <a01074882716@gmail.com>
2023-04-13 13:48:32 +0900

    2023/04/13

---
+++ Ch07/src/components/sub3/JwtComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <h4>JWT 실습하기</h4>
+  
+    <LoginForm></LoginForm>
+  </template>
+  <script setup>
+  import LoginForm from "./LoginForm.vue";
+  </script>
+  <style scoped></style>
